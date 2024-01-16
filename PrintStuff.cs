e4a21660f89ab94c4f5395eae4996408a8135c15
stevenarmstrong <stevenarmstrong@STEVENARMST258F>
2024-01-16 15:45:09 -0700

    Add project files.

---
+++ PrintStuff.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreCSharpFun
+{
+    internal class PrintStuff
+    {
+        //constructor
+        public PrintStuff(string language) {
+            language = temp;
+        }
+        public string PrintName(string n)
+        {
+            if (language == "en")
+            {
+                string output = "";
+
+                output = "Hello " + n + "!";
+
+                return output;
+            }
+            else
+            {
+                System.Console.WriteLine("Oi " + n + "!");
+            }
+           
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using MoreCSharpFun;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PrintStuff ps = new PrintStuff("pt");
+
+        string name = "";
+
+        System.Console.WriteLine("Please write your name: ");
+
+        name = System.Console.ReadLine();
+
+        ps.PrintName(name);
+    }
+}
